4ea6c7c6ca07cb76ace658e04e18f5b852259757
sofee <sofya009@gmail.com>
2016-06-01 23:02:09 +0300

    trying marionette

--- js/main.js
+++ js/main.js
@@ -1,12 +1,37 @@
 (function() {
     'use strict';
 
-    var app = Marionette.Application.extend({
-    	initialize: function (options) {
-    		console.log(options.test);
-    	}
+    // var app = Marionette.Application.extend({
+    // 	initialize: function () {
+    // 		console.log(1);
+    // 	}
+    // });
+
+    var AeroLayoutView = Marionette.LayoutView.extend({
+        el: 'tr.template_place',
+        template: "#layout-view-template"
+
+        // regions: {
+        //     flot: '#flot',
+        //     reis: '#reis',
+        //     country: '#country',
+        //     count_reis: '#count_reis'
+        // }
     });
 
+    var layoutView = new AeroLayoutView({
+        model: new Backbone.Model({
+            aero: [{
+                flot: 'FlyMSK',
+                reis: '555 B',
+                country: 'РОссия',
+                count_reis: 5
+            }]
+        })
+    });
 
-    var MyApp = new app({test: 'test'});
+    layoutView.render();
+
+
+    // var MyApp = new app();
 })();
