643f7bbf7cd7efd2e96fd0e5878abe427894d274
sofee <sofya009@gmail.com>
2016-06-01 12:24:59 +0300

    start js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 (function() {
     'use strict';
 
+    var app = Marionette.Application.extend({
+    	initialize: function (options) {
+    		console.log(options.test);
+    	}
+    });
 
-    
 
+    var MyApp = new app({test: 'test'});
 })();
