20d23ddf6b5c03bb27e45b66ab92da9e2388fc31
sofee <sofya009@gmail.com>
2016-06-02 17:25:10 +0300

    start marionette

--- js/main.js
+++ js/main.js
@@ -1,82 +1,78 @@
 (function() {
     'use strict';
 
-    // var AeroModel = Backbone.Model.extend({
-    //     defaults: function () {
-    //         return {
-    //             flot: '',
-    //             reis: '',
-    //             country: '',
-    //             count_reis: ''
-    //         };
-    //     }
+    var projectAero = new Backbone.Marionette.Application();
+
+
+    var AeroModel = Backbone.Model.extend({
+        defaults: function () {
+            return {
+                flot: '',
+                reis: '',
+                country: '',
+                count_reis: ''
+            };
+        }
+    });
+
+    var AeroItemView = Marionette.ItemView.extend({ //a view that represents a single item
+        tagName: 'tr',
+        template: "#layout-view-template"
+    });
+
+    var AeroCollection = Backbone.Collection.extend({
+        model: AeroModel
+    });
+
+    var AeroCollectionView = Marionette.CollectionView.extend({ //render one ItemView per model
+        el: 'tbody',
+        childView: AeroItemView
+    });
+
+    var AeroCompositeView = Marionette.CompositeView.extend({ //use this view to render our collection
+        
+    });
+
+    var AeroLayoutView = Marionette.LayoutView.extend({  //предназначен для организации иерархий представлений
+
+    });
+
+    
+    projectAero.addRegions({
+        mainRegions: '.wrapp_page'
+    });
+
+    // var aero = [{
+    //             flot: 'FlyMSK',
+    //             reis: '555 B',
+    //             country: 'РОссия',
+    //             count_reis: 5
+    //         },
+    //         {
+    //             flot: 'FlyMSK1',
+    //             reis: '555 B1',
+    //             country: 'РОссия1',
+    //             count_reis: 51
+    //         }];
+
+    var myAero = new AeroCollection(AeroModel);
+    var myAeroView = new AeroCollectionView({collection: myAero});
+    myAeroView.render();
+
+    $('tbody').append(myAeroView.el);
+    // var layoutView = new AeroItemView({
+    //     model: new Backbone.Model({
+    //         aero: [{
+    //             flot: 'FlyMSK',
+    //             reis: '555 B',
+    //             country: 'РОссия',
+    //             count_reis: 5
+    //         }]
+    //     })
     // });
 
-    // var AeroItemView = Marionette.ItemView.extend({
-    //     tagName: 'tr',
-    //     template: "#layout-view-template"
-    // });
-
-    // var AeroCollection = Backbone.Collection.extend({
-    //     model: AeroModel
-    // });
-
-    // var AeroCollectionView = Backbone.Marionette.CollectionView.extend({
-    //     el: 'tbody',
-    //     childView: AeroItemView
-    // });
-
-    // var AeroAll = Marionette.CompositeView.extend({
-    //     el: '.wrapp_page',
-    //     childView: AeroModel,
-    //     ui: {
-    //         form: 'form',
-    //         name: '#name_flot',
-    //         reis: '#reise_flot'
-    //     },
-    //     triggers: {
-    //         'submit @ui.form': 'add:new:line'
-    //     },
-    //     onAddNewLine: function (event){
-    //         console.log(1);
-    //         // this.model.set({
-    //         //     flot: this.ui.name.val(),
-    //         //     reis: this.ui.reis.val()
-    //         // })
-    //     },
-    // });
-
-    // // var aero = [{
-    // //             flot: 'FlyMSK',
-    // //             reis: '555 B',
-    // //             country: 'РОссия',
-    // //             count_reis: 5
-    // //         },
-    // //         {
-    // //             flot: 'FlyMSK1',
-    // //             reis: '555 B1',
-    // //             country: 'РОссия1',
-    // //             count_reis: 51
-    // //         }];
-
-    // var myAero = new AeroCollection(AeroModel);
-    // var myAeroView = new AeroCollectionView({collection: myAero});
-    // myAeroView.render();
-
-    // $('tbody').append(myAeroView.el);
-    // // var layoutView = new AeroItemView({
-    // //     model: new Backbone.Model({
-    // //         aero: [{
-    // //             flot: 'FlyMSK',
-    // //             reis: '555 B',
-    // //             country: 'РОссия',
-    // //             count_reis: 5
-    // //         }]
-    // //     })
-    // // });
-
-    // // layoutView.render();
+    // layoutView.render();
 
 
-    // // var MyApp = new app();
+    // var MyApp = new projectAero();
 })();
